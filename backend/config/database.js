e22683f91756ee22fcab6a320cd88748439bb3d4
Vladimir_Oleinik <113135004+0mistakes@users.noreply.github.com>
2023-01-15 22:05:15 +0300

    full project

---
+++ backend/config/database.js
@@ -0,0 +1,20 @@
+import mysql from "mysql2";
+  
+// create the connection to database
+const db = mysql.createConnection({
+host : '127.0.0.1',
+port: '3306',
+user : 'root',
+password : 'root',
+database: 'pos_db'
+});
+
+db.connect( function (err) { 
+    if (err) { 
+        console.log("!!! Cannot connect !!! Error:"); throw err; 
+    } else { 
+        console.log("Connection established."); 
+    } 
+});
+ 
+export default db;
